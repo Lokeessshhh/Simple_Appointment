6c15228fc09c6f02480e39bec2a0019ea2eba3ab
Lokeessshhh <lokeshhlohar80@gmail.com>
2026-06-05 09:18:19 +0530

    Configure Django admin for Appointment model

--- reminders/admin.py
+++ reminders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'phone_number', 'appointment_datetime', 'confirmation_sent', 'reminder_sent')
+    list_filter = ('confirmation_sent', 'reminder_sent', 'appointment_datetime')
+    search_fields = ('customer_name', 'phone_number')
+    readonly_fields = ('created_at',)
